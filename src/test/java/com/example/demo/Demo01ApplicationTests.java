356aa121422b59ca8c09ec91ec59f79c52e7678e
yamas <yamamoto.satoshi@isid.co.jp>
2017-11-20 10:43:02 +0900

    first

---
+++ src/test/java/com/example/demo/Demo01ApplicationTests.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo01ApplicationTests {
+	TestRestTemplate restTemplate = new TestRestTemplate();
+	@LocalServerPort
+	int port;
+	
+	@Test
+	public void contextLoads() {
+	//	assertThat(restTemplate.getForObject("http://localhost:" + port,String.class), is("hoge"));
+		
+	}
+
+}
